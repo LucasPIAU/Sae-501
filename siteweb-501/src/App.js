d53cf16eb6d1123ba804db3b289a5633083984f0
GGab1 <149067546+GGab1@users.noreply.github.com>
2024-11-15 16:24:24 +0100

    Header fini

--- siteweb-501/src/App.js
+++ siteweb-501/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
-import Header from './components/Header.jsx';
+import Header from './components/MainHeader.jsx';
+import Filtrage from './components/FilterFilieres.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <p>SALE MEEEEEERDE</p>
+      <Filtrage/>
     </div>
   );
 }
