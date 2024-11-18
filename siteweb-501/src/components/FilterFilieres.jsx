5e02293154b4b7be3266f8cd38a9ce6a6a6d727d
GGab1 <149067546+GGab1@users.noreply.github.com>
2024-11-18 10:07:00 +0100

    Début du filtrage sur les filières

--- siteweb-501/src/components/FilterFilieres.jsx
+++ siteweb-501/src/components/FilterFilieres.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const FilterFiliere = () => {
-    
+    return (
+        <>
+            <div className='filtre-filiere'>
+                <input type="text" id="key" name="key"/>
+                <fieldset>
+                    <legend>Filière</legend>
+
+                    <div>
+                        <input type="radio" id="g&t" name="filiere" value="g&t" checked />
+                        <label for="g&t">Général et Technologique</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" id="pro" name="filiere" value="pro" />
+                        <label for="pro">Professionnelle</label>
+                    </div>
+                </fieldset>
+            </div>
+        </>
+    )
 }
 
 export default FilterFiliere;
