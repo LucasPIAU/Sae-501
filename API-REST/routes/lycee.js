03605e7f6f08ff1ec6f29bda00a091f66ce8a589
Lucas PIAU <Lucas.Piau.Etu@univ-lemans.fr>
2024-11-19 09:45:43 +0100

    Ajout du fichier patch-note et ajout de new routes

--- API-REST/routes/lycee.js
+++ API-REST/routes/lycee.js
@@ -2,12 +2,58 @@ const express = require('express');
 
 const lyceeRoutes = express();
 
+// Routes GET
+
 lyceeRoutes.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        
+        // Récupération d'un lycée
+        res.status(200).json('OK');
     } catch (err) {
-        res.status(500).json({message: `Une erreur interne est survenue dans la récupération d'un lycee : ${err}`});
+        res.status(500).json({message: `Une erreur interne est survenue dans la récupération d'un lycée : ${err}`});
+    }
+});
+
+lyceeRoutes.get('', async (req, res) => {
+    try {
+        // Récupération de la liste de tout les lycées
+        res.status(200).json('OK');
+    } catch (err) {
+        res.status(500).json({message: `Une erreur interne est survenue dans la récupération des lycées : ${err}`});
+    }
+});
+
+// Routes PUT
+
+lyceeRoutes.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        // Modification d'un lycée avec les nouvelles données
+        res.status(200).json('OK');
+    } catch (err) {
+        res.status(500).json({message: `Une erreur est survenue pendant la modification d'un lycée : ${err}`});
+    }
+});
+
+// Routes POST
+
+lyceeRoutes.post('/add', async (req, res) => {
+    try {
+        // Création d'un lycée' avec les données
+        res.status(200).json('OK');
+    } catch (err) {
+        res.status(500).json({message: `Une erreur est survenue pendant la création d'un lycée : ${err}`});
+    }
+});
+
+// Routes DELETE
+lyceeRoutes.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        // Suppression dun lycée avec les nouvelles données
+        res.status(200).json('OK');
+    } catch (err) {
+        res.status(500).json({message: `Une erreur est survenue pendant la suppression d'un lycée : ${err}`});
     }
 });
 
