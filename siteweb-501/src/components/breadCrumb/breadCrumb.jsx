65517b1ced994a7bb0621711f6b3444476bbe541
Axel Bone-Tellier <Axel.Bone--tellier.Etu@univ-lemans.fr>
2025-01-09 08:51:42 +0100

    breadCrumb

---
+++ siteweb-501/src/components/breadCrumb/breadCrumb.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { BreadcrumbContext } from "../../App";
+
+const BreadCrumb = () => {
+  const { breadcrumbs } = useContext(BreadcrumbContext);
+
+  return (
+    <nav>
+      {breadcrumbs.map((crumb, index) => (
+        <span key={crumb.path}>
+          {index > 0 && " > "}
+          <Link to={crumb.path}>{crumb.label}</Link>
+        </span>
+      ))}
+    </nav>
+  );
+};
+
+export default BreadCrumb;
